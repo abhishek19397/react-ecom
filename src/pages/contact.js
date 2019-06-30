541a5caa7c9ac0e7ff95c90edaeeba1511a67bd3
Abhishek Tiwari <dev.abhishekt@gmail.com>
2019-06-30 12:02:24 +0530

    dummy contact

---
+++ src/pages/contact.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from '../components/Reusable/HeroSection'
+import Infoblock from '../components/Reusable/Infoblock'
+import DualInfoblock from '../components/Reusable/DualInfoblock'
+import CourseCart from '../components/Cart/CourseCart'
+
+const IndexPage = ({data}) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection
+     img={data.img.childImageSharp.fluid}
+     title="I write Code"
+     subtitle="Easiest way to learn programming"
+     heroclass="hero-background"  
+    />  
+    <Infoblock heading="About us" />
+    <CourseCart courses={data.mycourses} />
+     <DualInfoblock heading="Our team" /> 
+  </Layout>
+)
+
+export const query = graphql`
+ {
+   img:file(relativePath: {eq:"heromain.png"}) {
+     childImageSharp{
+       fluid{
+         ...GatsbyImageSharpFluid_tracedSVG
+       }
+     }
+   },
+   mycourses:allContentfulCourses{
+     edges{
+       node{
+         id
+         title
+         price
+         category
+         description{
+           description
+         }
+         image{
+           fixed(width:200,height:120){
+            ...GatsbyContentfulFixed_tracedSVG
+           }
+         }
+       }
+     }
+   }
+ } 
+`
+
+export default IndexPage
